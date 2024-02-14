2421a66eef2c80f33039276784738b7165ec5141
MironovNick <mironov.nik88@gmail.com>
2024-02-14 17:03:43 +0300

    add files

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -23,27 +23,27 @@ export const HW3 = () => {
     const handleSave = () => {
         // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
         // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
+        const newValue = currentText
+        setTexts([newValue, ...texts])
+        setCurrentText("")
     };
 
     return (
-        <div id={'hw03'}>
+        <div id={'hw03'}>{
             currentText ? (
-            <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+            <h1 id={'hw03-text'}>{currentText}</h1>
             ) : (
-            <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> {/* ничего не меняем, здесь все норм */}
+            <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1>
             )}
 
             <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange}/>
 
-            <button id={'hw03-button'} onClick={() => {
-            }}> {// НЕ ХВАТАТЕТ ФУНКЦИИ //}
-                {/*  Сохранить*/}
-            </button>
+            <button id={'hw03-button'} onClick={handleSave}> Сохранить </button>
 
             <h1 style={{marginTop: '50px'}}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-            <ol id={'hw03-tasks'}>*/}
-                {{//ОТРИСОВАТЬ МАССИВ//}.map((el, index) => {
+            <ol id={'hw03-tasks'}>
+                {texts.map((el, index) => {
                     return (
                     <li key={index} id={`hw03-task-${index}`}>
                 {el}
